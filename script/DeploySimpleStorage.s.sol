c9d2fcbc4d0b3965e0d2d0bb804470b05bb8d84a
rayhanalmim <rayhanalmim1@gmail.com>
2025-01-09 12:39:33 +0600

    ADDED INFO IN .ENV

--- script/DeploySimpleStorage.s.sol
+++ script/DeploySimpleStorage.s.sol
@@ -1,6 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.28;
+import {Script} from "../lib/forge-std/src/Script.sol";
+import {SimpleStorage} from "../src/SimpleStorage.sol";
 
-contract DeploySimpleStorage {
-    
+contract DeploySimpleStorage is Script {
+    function run() external {
+        // uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast();
+
+        new SimpleStorage();
+        
+        vm.stopBroadcast();
+    }
 }
