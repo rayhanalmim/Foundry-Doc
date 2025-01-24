bec94d22a7f51364da6707fdb5de8c11c9c81f3e
Rayhan Al Mim <119398683+rayhanalmim@users.noreply.github.com>
2025-01-24 23:07:48 +0600

    Update SimpleStorage.sol

--- src/SimpleStorage.sol
+++ src/SimpleStorage.sol
@@ -1,10 +1,7 @@
-// I'm a comment!
+
 // SPDX-License-Identifier: MIT
 
 pragma solidity 0.8.28;
-
-// pragma solidity ^0.8.0;
-// pragma solidity >=0.8.0 <0.9.0;
 
 contract SimpleStorage {
     uint256 myFavoriteNumber;
